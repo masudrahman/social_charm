591ce2a3a656211f5966e300b04e03cb57734f0a
Masud Rahman <masudrahman95@gmail.com>
2017-02-17 11:34:15 -0800

    login and rate_others pages added

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+
+@app.route('/rate_others')
+def rate():
+	return render_template('rate_others.html')
