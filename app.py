0ee48029539e78af4655ebb033e9a74931bd3723
Masud Rahman <masudrahman95@gmail.com>
2017-04-12 17:36:22 -0700

    Won't use MySQL for now

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask
 from flask import render_template
-from flaskext.mysql import MySQL
+#from flaskext.mysql import MySQL
  
-mysql = MySQL()
+"""mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'social_charm'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+mysql.init_app(app)"""
 
 
 @app.route('/')
